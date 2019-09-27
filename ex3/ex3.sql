f25f90177fdaf47b1ab324251583ab8559bd507c
Sean DMR <sean@flyinggrizzly.net>
2019-09-27 16:11:44 +0100

    Add code and db for ex3

---
+++ ex3/ex3.sql
@@ -0,0 +1,8 @@
+insert into person (id, first_name, last_name, age)
+  values (0, 'Sean', 'DMR', 20);
+
+insert into pet (id, name, breed, age, dead)
+  values (0, 'Good Dog Brant', 'Doggo', 2, false);
+
+insert into person_pet (person_id, pet_id)
+  values (0, 0);
