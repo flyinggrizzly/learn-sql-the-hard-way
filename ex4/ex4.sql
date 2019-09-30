e66424c2125f35330adb7552e19eaf4824ae36d1
Sean DMR <sean@flyinggrizzly.net>
2019-09-30 09:10:50 +0100

    Add code and db for ex4

---
+++ ex4/ex4.sql
@@ -0,0 +1,4 @@
+insert into pet (id, name, breed, age, dead) values (1, 'Dang Robot', 'ROBOT', 21, 1);
+
+insert into person_pet (person_id, pet_id) values (0, 0);
+insert into person_pet values (0, 1);
